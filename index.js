dc749538871203c7966586e36694c1e1a5a9a879
Natalia <natalia.datascience@gmail.com>
2019-11-30 09:47:11 -0300

    caution message positioning

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-request('aframe')
+require('aframe')
+require('aframe-event-set-component');
+
+function tooltip(obj, objName){
+    console.log('name', name);
+    console.log('object', obj);
+}
+
+var dictObjName ={
+    drill: "Furadeira",
+    tripod: "Tripe",
+    door: "Porta",
+    whiteboard: "Quadro de recados",
+    drawing: "Desenho",
+    instructions: "Instrucoes"
+}
+
