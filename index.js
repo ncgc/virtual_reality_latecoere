1c6b73f84f560b03c2933584661a7e18a1aaee0f
Natalia <natalia.datascience@gmail.com>
2019-12-02 02:55:03 -0300

    zoom improvement

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ AFRAME.registerComponent ('zoom', {
 	init: function () {
 		var el = this.el;
         var data = this.data;
-        var id = el.id;
-        
+		
 		el.addEventListener('click', function () {			
-			if (id=="doordrawn") {
+			if (data.target) {
+                console.log(data.target);
                 var currentScale = el.getDOMAttribute('scale');
-                
+                var id = el.id;
                 if(currentScale.x !=2){
                     console.log("devo dar zoom");
                     el.setAttribute('scale', {x:2, y:2, z: 2});
@@ -19,31 +19,20 @@ AFRAME.registerComponent ('zoom', {
                     console.log(el.getDOMAttribute('scale'));
                 }
                 else{
-                    console.log("sou porta e devo voltar ao normal");
-                    el.setAttribute('position', {x:-1.1, y:1.65, z:-1.5});
-                    el.setAttribute('scale', {x:1, y:0.8, z:1});
-                    console.log(el.getDOMAttribute('scale'));
-                    }
-                }
-            
-            if(id == "techdrawn"){
-                var currentScale = el.getDOMAttribute('scale');
-                
-                if(currentScale.x !=2){
-                    console.log("devo dar zoom");
-                    el.setAttribute('scale', {x:2, y:2, z: 2});
-                    el.setAttribute('position', {x:0 ,y:1.65, z: 3});
-                    console.log(el.getDOMAttribute('scale'));
-                }
-                else{
-                   
+                    if(id == "doordrawn"){
                         console.log("sou porta e devo voltar ao normal");
-                        el.setAttribute('position', {x:0.4, y:1.65, z:-1.5});
-                        el.setAttribute('scale', {x:1, y:0.6, z:1});
+                        el.setAttribute('position', {x:-1.1, y:1.65, z:-1.5});
+                        el.setAttribute('scale', {x:1, y:0.8, z:1});
+                        console.log(el.getDOMAttribute('scale'));
+                    }
+                    if(id == "techdrawn"){
+                        console.log("sou tech e devo voltar ao normal");
+                        el.setAttribute('position', {x: 0.4, y: 1.65, z: -1.5});
+                        el.setAttribute('scale', {x:1, y: 0.6, z:1});
                         console.log(el.getDOMAttribute('scale'));
                     }
                 }
-            			
+			}			
 		});
 	}
 });
